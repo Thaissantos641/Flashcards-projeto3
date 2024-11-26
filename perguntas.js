06054edfa5958ec7bb75e063a8d68982f14bd421
Thaissantos641 <00001117663668sp@al.educacao.sp.gov.br>
2024-11-26 18:20:06 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'O que é Phyton?',
+    'O Python é uma linguagem de programação.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o menor país do mundo?',
+    'O menor país do mundo é o Vaticano.'
+)
+
+criaCartao(
+    'Conhecimentos Gerais',
+    'Quantos dias tem um ano bissexto?',
+    'Um ano bissexto tem 366 dias.'
+)
+
+criaCartao(
+    'Português',
+    'O que é um adjetivo?',
+    'Adjetivo é uma classe gramatical que classifica e qualifica os substantivos que o acompanham.'
+)
+
+criaCartao(
+    'Ciências',
+    'Qual é o planeta mais próximo do Sol?',
+    'O planeta mais próximo do Sol é Mercúrio.'
+)
+
+criaCartao(
+    'Filosofia',
+    'De quem é a famosa frase "Penso, logo existo"?',
+    'Essa frase é do filósofo René Descartes (1596-1650).'
+)
